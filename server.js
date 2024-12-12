c891080c1946163adf544151155d1d538ed9e535
dung0811 <103187128+dung2511@users.noreply.github.com>
2024-12-12 13:56:39 +0700

    backend-zero video 13

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1'; // localhost || sử dụng địa chỉ IP: '127.0.0.1' or 'localhost'
 const port = 3000;
 
+
+// Tạo server website
 const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\nDev');
 });
 
+
+// Website chạy ở đâu 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
